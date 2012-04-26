80d3eb038495288315e46262b86c08ef98cf4344
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-04-26 13:10:32 +0000

    #327 Se cambia table logged por table loggeable
    git-svn-id: svn://saruman/admportal/trunk@20 32ebc6c2-cfed-11df-be44-b108719c1499

---
+++ library/Zwei/Db/TableLoggeable.php
@@ -0,0 +1,37 @@
+<?php
+class Zwei_Db_TableLoggeable extends Zwei_Db_Table
+{
+    public function insert($data)
+    {
+    	$last_insert_id = parent::insert($data);  
+        if ($last_insert_id) $this->log("ADD", $data, $last_insert_id);   
+        return true;
+    }
+    
+    public function update($data, $where)
+    {
+        $this->log("EDIT", $data, $where);
+        return parent::update($data, $where);
+    }
+    
+    public function delete($where)
+    {
+        $this->log("DELETE", false, $where);
+        return parent::delete($where);
+    }   
+    
+    public function log($action, $data, $condition=false) {
+        if (self::$_defaultLogMode) {
+            $logBook = new LogBookModel();
+            
+            $logData = array (
+                "user"=>$this->_user_info->user_name,
+                "table"=> $this->_name,
+                "action"=>$action
+            ); 
+            
+            if ($condition) $logData["condition"] = (string) $condition; 
+            $logBook->insert($logData);
+        }       
+    }   
+}
