ced5d6d41a91eeaed0c6bd6a9dc651582d630a87
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-05-04 22:12:39 +0000

    #327 dojo password
    git-svn-id: svn://saruman/admportal/trunk@49 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Admin/Elements/Password.php
+++ library/Zwei/Admin/Elements/Password.php
@@ -9,8 +9,8 @@
  * @since 0.1
  */
 class Zwei_Admin_Elements_Password extends Zwei_Admin_Elements_Element{
-	function edit($i,$j,$display="inline"){
-		return "<input type=\"password\" style=\"display:$display\" id=\"edit{$i}_{$j}\" name=\"$this->target[$i]\" value=\"{$this->value}\" />";
+	function edit($i,$j,$display="block"){
+		return "<input dojoType=\"dijit.form.ValidationTextBox\" type=\"password\" style=\"display:$display\" id=\"edit{$i}_{$j}\" name=\"$this->target[$i]\" value=\"{$this->value}\" />";
 	}
 }
 
