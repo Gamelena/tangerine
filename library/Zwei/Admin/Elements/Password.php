dcd6789509a594c6240427f5a6ad0dfcaa0d10c1
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-05-14 14:08:21 +0000

    #327 upgrade Zwei_Admin_Elements_Password,  acepta readonly, disabled, required, onblur, reg_exp, invallid_message, prompt_message, maxlenght, trim
    git-svn-id: svn://saruman/admportal/trunk@69 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Admin/Elements/Password.php
+++ library/Zwei/Admin/Elements/Password.php
@@ -9,8 +9,19 @@
  * @since 0.1
  */
 class Zwei_Admin_Elements_Password extends Zwei_Admin_Elements_Element{
-	function edit($i,$j,$display="block"){
-		return "<input dojoType=\"dijit.form.ValidationTextBox\" type=\"password\" style=\"display:$display\" id=\"edit{$i}_{$j}\" name=\"$this->target[$i]\" value=\"{$this->value}\" />";
+	function edit($i,$j,$display="block")
+	{
+	    $readonly = @$this->params['READONLY'] ? "readonly=\"readonly\"" : '';
+        $disabled = @$this->params['DISABLED'] ? "disabled=\"disabled\"" : '';
+        $required = @$this->params['REQUIRED'] ? "required=\"true\"" : '';
+        $onblur = @$this->params['ONBLUR'] ? "onblur=\"{$this->params['ONBLUR']}\"" : '';
+        $regexp = isset($this->params['REG_EXP']) ? "RegExp=\"{$this->params['REG_EXP']}\"" : '';
+        $invalid_message= isset($this->params['INVALID_MESSAGE']) ? "invalidMessage=\"{$this->params['INVALID_MESSAGE']}\"" : '';
+        $prompt_message= isset($this->params['PROMPT_MESSAGE']) ? "promptMessage=\"{$this->params['PROMPT_MESSAGE']}\"" : '';
+        $maxlength = isset($this->params['MAXLENGTH']) ? "maxlength=\"{$this->params['MAXLENGTH']}\"" : '';
+        $trim = isset($this->params['TRIM']) ? "trim=\"true\"" : '';
+	    
+		return "<input dojoType=\"dijit.form.ValidationTextBox\" type=\"password\" style=\"display:$display\" id=\"edit{$i}_{$j}\" name=\"$this->target[$i]\" value=\"{$this->value}\" $readonly $disabled $required $regexp $trim $invalid_message $prompt_message $maxlength $onblur />";
 	}
 }
 
