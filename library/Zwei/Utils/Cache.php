ed3248e03f1a4a268df6dc2f420f9c9f373984eb
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-05-24 18:00:02 +0000

    #327 se cachea json de configuraci?\195?\179n y arbol por perfil
    git-svn-id: svn://saruman/admportal/trunk@85 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Utils/Cache.php
+++ library/Zwei/Utils/Cache.php
@@ -6,71 +6,79 @@
  */
 final class Zwei_Utils_Cache
 {
-	/**
-	 * 
-	 * @var array
-	 */
-	protected $_backendOpt;
     /**
-	 * 
-	 * @var array
-	 */
-	protected $_frontendOpt;
-	
-	/**
-	 * 
-	 * @var Zend_Cache_Output
-	 */
-	protected $_cache;
-	/**
-	 * 
-	 * @var boolean
-	 */
-	protected $_isStarted = false;
-	/**
-	 * 
-	 * @var boolean
-	 */
-	protected $_check = false;
-	
+     * 
+     * @var array
+     */
+    protected $_backendOpt;
+    /**
+     * 
+     * @var array
+     */
+    protected $_frontendOpt;
+    
+    /**
+     * 
+     * @var Zend_Cache_Output
+     */
+    protected $_cache;
+    /**
+     * 
+     * @var boolean
+     */
+    protected $_isStarted = false;
+    /**
+     * 
+     * @var boolean
+     */
+    protected $_check = false;
+    
     public function __construct($backendOpt, $frontendOpt) 
     {
-    	$this->_backendOpt = $backendOpt;
-    	$this->_frontendOpt = $frontendOpt;
+        $this->_backendOpt = $backendOpt;
+        $this->_frontendOpt = $frontendOpt;
     }
 
     public function start() 
     {
-		if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] == "/components" 
-		  && Zend_Auth::getInstance()->hasIdentity() 
-		  && !preg_match("/grafico/", @$_REQUEST['p'])) {
-		  	Debug::write($_SERVER['PATH_INFO']);
-		  	$this->_check = true;
-		    $userInfo = Zend_Auth::getInstance()->getStorage()->read();
-		    $cacheid = md5( $userInfo->acl_roles_id.@$_REQUEST['p'] ); 
-		    // make object
-		    $this->_cache = Zend_Cache::factory('Output',
-		                                 'File',
-		                                 $this->_frontendOpt,
-		                                 $this->_backendOpt);
-		
-		    $this->_isStarted = $this->_cache->start($cacheid);
-		} 
-    	
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            $userInfo = Zend_Auth::getInstance()->getStorage()->read();
+            $pathInfo = isset($_SERVER['PATH_INFO']) ? isset($_SERVER['PATH_INFO']) : '';
+            if ($pathInfo == "/components" && !preg_match("/grafico/", @$_REQUEST['p']))
+            {
+                $this->_check = true;
+                $userInfo = Zend_Auth::getInstance()->getStorage()->read();
+                $cacheid = md5($userInfo->acl_roles_id.@$_REQUEST['p']); 
+                // make object
+                $this->_cache = Zend_Cache::factory('Output', 'File', $this->_frontendOpt, $this->_backendOpt);
+                $this->_isStarted = $this->_cache->start($cacheid);
+            } else if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] == "model=settings&format=json") {
+                $cacheid = $userInfo->acl_roles_id."settings"; 
+                $this->_check = true;
+                $this->_cache = Zend_Cache::factory('Output', 'File', $this->_frontendOpt, $this->_backendOpt);
+                $this->_isStarted = $this->_cache->start($cacheid);
+            } else if ($pathInfo == "/modules") {
+                $cacheid = $userInfo->acl_roles_id."modules"; 
+                $this->_check = true;
+                $this->_cache = Zend_Cache::factory('Output', 'File', $this->_frontendOpt, $this->_backendOpt);
+                $this->_isStarted = $this->_cache->start($cacheid);             
+            }
+        } 
+        
     }
     
     public function check()
     {
-    	return $this->_check;
+        return $this->_check;
     }
     
     public function isStarted()
     {
-    	return $this->_isStarted;
+        return $this->_isStarted;
     }
     
     public function end()
     {
-    	$this->_cache->end();
+        $this->_cache->end();
     }
 }
