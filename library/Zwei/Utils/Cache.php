d0cfb61502f33cdf529e1f3b07d2af86dd552c0e
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-05-24 23:00:23 +0000

    #327 se sacan graficos de cache
    git-svn-id: svn://saruman/admportal/trunk@92 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Utils/Cache.php
+++ library/Zwei/Utils/Cache.php
@@ -41,7 +41,7 @@ final class Zwei_Utils_Cache
 
     public function start() 
     {
-        if (Zend_Auth::getInstance()->hasIdentity()) {
+        if (Zend_Auth::getInstance()->hasIdentity() && !isset($_REQUEST['action']) && !preg_match("/grafico/", @$_REQUEST['p'])) {
             $userInfo = Zend_Auth::getInstance()->getStorage()->read();
             $pathInfo = isset($_SERVER['PATH_INFO']) ? isset($_SERVER['PATH_INFO']) : '';
             if ($pathInfo == "/components" && !preg_match("/grafico/", @$_REQUEST['p']))
