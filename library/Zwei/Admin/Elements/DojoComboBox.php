6862169748b0bbac0f70b7d8a9614a7c9bc09723
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-09-28 14:48:42 +0000

    #327 se a?\195?\177ade soporte para param "disabled" en dijit.Form.FilteringSelect
    git-svn-id: svn://saruman/admportal/trunk@191 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Admin/Elements/DojoComboBox.php
+++ library/Zwei/Admin/Elements/DojoComboBox.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * Combobox Dojo
- *
- *
+ * Dojo Filtering Select
+ * 
+ * 
  * @category Zwei
  * @package Zwei_Admin
  * @subpackage Elements
@@ -11,56 +12,122 @@
  */
 
 
-class Zwei_Admin_Elements_DojoComboBox extends Zwei_Admin_Elements_Element{
+class Zwei_Admin_Elements_DojoFilteringSelect extends Zwei_Admin_Elements_Element
+{
+    function edit($i, $j, $display="block")
+    {
+        $required = isset($this->params['REQUIRED']) ? "required=\"true\"" : "required=\"false\"";
+        $onchange = isset($this->params['ONCHANGE']) ? "onchange=\"{$this->params['ONCHANGE']}\"":'';
+        $regexp = isset($this->params['REG_EXP']) ? "RegExp=\"{$this->params['REG_EXP']}\"" : '';
+        $invalid_message = isset($this->params['INVALID_MESSAGE']) ? "invalidMessage=\"{$this->params['INVALID_MESSAGE']}\"" : '';
+        $prompt_message = isset($this->params['PROMPT_MESSAGE']) ? "promptMessage=\"{$this->params['PROMPT_MESSAGE']}\"" : '';
+        $value =  isset($this->params['DEFAULT_VALUE']) && !isset($this->params['DEFAULT_TEXT']) ? "value=\"{$this->params['DEFAULT_VALUE']}\"" : '';
+        $options = $this->options();
+        $return =  "<select dojoType=\"dijit.form.ComboBox\" id=\"edit{$i}_{$j}\" name=\"$this->target[$i]\" onload=\"dijit.byId('edit{$i}_{$j}').set('value', dijit.byId('edit{$i}_{$j}').get('value'))\" $value $onchange $required $regexp $invalid_message $prompt_message style=\"display:$display\" >\r\n$options\r\n</select>\r\n
+        ";
+        return $return;
+    }
+  
+    function display($i, $j)
+    {
+        $request = new Zwei_Utils_Form();
+    
+        if ($this->value == null) {
+            $value = (isset($request->{$this->target})) ? $request->{$this->target} : null;
+        } else {
+            $value = $this->value;
+        }
+        
+        
+        $ClassModel = Zwei_Utils_String::toClassWord($this->params['TABLE'])."Model";   
+        $model = new $ClassModel;
+        $id = !empty($this->params['TABLE_PK']) ? $this->params['TABLE_PK'] : $model->getName().'.id';
+        
+        $field = (isset($this->params['TABLE_FIELD'])) ? $this->params['TABLE_FIELD'] : $this->params['FIELD'];
+        
+        
+        if (isset($this->params['DEFAULT_VALUE']) && $value == $this->params['DEFAULT_VALUE']) {
+            return isset($this->params['DEFAULT_TEXT']) ? $this->params['DEFAULT_TEXT'] : "Ninguno";
+        } else if (!empty($this->params['TABLE_METHOD'])) {
+            $method = Zwei_Utils_String::toFunctionWord($this->params['TABLE_METHOD']);
+            $select = $model->$method();
+        } else { 
+            $select = $model->select(array($field, $id));
+        }
 
-	function edit($i, $j, $display="block"){
-		$options=$this->options();
-		return "<select dojoType=\"dijit.form.ComboBox\" style=\"display:$display\" id=\"edit{$i}_{$j}\" name=\"{$this->target}[$i]\">\r\n$options\r\n</select>";
-	}
+        $select->where($model->getAdapter()->quoteInto("$id = ? ", $value));
+        Zwei_Utils_Debug::writeBySettings($select->__toString(), 'query_log');   
+        $rows = $model->fetchAll($select);
+                
+        return isset($rows[0]) ? $rows[0][$this->params['FIELD']] : ''; 
+    }
 
+    function options() 
+    {
+        
+        $options = "";
 
-	function display($i, $j){
-		$options=$this->options();
-		return "<select dojoType=\"dijit.form.ComboBox\" disabled=\"disabled\" id=\"field{$i}_{$j}\">\r\n$options\r\n</select>";
-	}
+        $selected = array();
+        
+        if (!empty($this->params['TABLE'])) {
+            $id =! empty($this->params['TABLE_PK'])?$this->params['TABLE_PK'] : 'id';
+            $ClassModel = Zwei_Utils_String::toClassWord($this->params['TABLE'])."Model";
+            $model = new $ClassModel;
+            
+            if (!empty($this->params['TABLE_METHOD'])) {
+                $method = Zwei_Utils_String::toFunctionWord($this->params['TABLE_METHOD']);
+                $select = $model->$method();
+            } else {
+                if (!empty($this->params['TABLE_FIELD'])) {
+                    $select = $model->select(array($this->params['TABLE_FIELD'], $id));
+                } else if (!empty($this->params['FIELD'])){
+                    $select = $model->select(array($this->params['FIELD'], $id));
+                } else {
+                    $select = $model->select(array("title", $id));                  
+                }   
+            }
+            Zwei_Utils_Debug::writeBySettings($select->__toString(), 'query_log');
+            $rows = $model->fetchAll($select);
+            $request = new Zwei_Utils_Form();
+        
+            if ($this->value == null) {
+                $value = (isset($request->{$this->target})) ? $request->{$this->target} : null;
+            } else {
+                $value = $this->value;
+            }
 
-	function options(){
-		$options="<option value=\"0\">* None *</option>\r\n";
-		//$dao=new DAO();
-		$id=!empty($this->params['TABLEPK'])?$this->params['TABLEPK']:'id';
-		$model=new $this->params['TABLE'];
-		//$model=new MenuModel();
+            if (isset($this->params['DEFAULT_VALUE']) && isset($this->params['DEFAULT_TEXT'])) {
+                $text = $this->params['DEFAULT_TEXT'];
+                $options .= "<option value=\"{$this->params['DEFAULT_VALUE']}\">$text</option>\r\n";
+            }
 
-		$select=$model->select();
-		$select->from($model, array($id, $this->params['FIELD']));
-
-		//$dao->query("SELECT `{$this->params['FIELD']}`,`$id` FROM {$this->params['TABLE']}");
-
-		$rows=$model->fetchAll($select);
-		$selected=array();
-
-
-		$request=new Zwei_Utils_Form();
-
-		if($this->value==null){
-			if(isset($request->{$this->target}))$value=$request->{$this->target};
-			else $value=null;
-		}else{
-			$value=$this->value;
-		}
-
-
-		//while($row=$dao->getAll()){
-		foreach($rows as $row){
-			$selected[$row[$id]] = $row[$id]==$value ? "selected" : "";
-			$options.="<option value=\"".$row[$id]."\" ".$selected[$row[$id]]." >{$row[$this->params['FIELD']]}</option>\r\n";
-		}
-
-		return $options;
-
-	}
-
-
-
+            
+            foreach ($rows as $row) {
+                $selected = $row[$id] == $this->value ? "selected" : "";
+                if (!empty($this->params['TABLE_FIELD'])) {
+                    $options .= "<option value=\"".$row[$id]."\" ".$selected." >{$row[$this->params['TABLE_FIELD']]}</option>\r\n";
+                } else if (!empty($this->params['FIELD'])) {
+                    $options .= "<option value=\"".$row[$id]."\" ".$selected." >{$row[$this->params['FIELD']]}</option>\r\n";
+                } else {
+                    $options .= "<option value=\"".$row[$id]."\" ".$selected." >{$row["title"]}</option>\r\n";
+                }   
+            }
+        } else {
+            if ($this->value == null) {
+                $value = (isset($request->{$this->target})) ? $request->{$this->target} : null;
+            } else {
+                $value = $this->value;
+            }
+            
+            $options = "";
+            $rows = explode(",", $this->params['LIST']);
+            foreach ($rows as $row) {
+                //Zwei_Utils_Debug::write('$id='.$id.'$row='.$row.'$value='.$value);
+                $selected = $row == $value ? "selected" : "";
+                $options .= "<option value=\"".$row."\" ".$selected." >$row</option>\r\n";
+            }
+            
+        }
+        return $options;
+    }
 }
-
