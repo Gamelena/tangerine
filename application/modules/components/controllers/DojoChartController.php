4e1d68f7060054d6123990fc210107fa41e74581
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-10-24 22:14:02 +0000

    #638 modulo graficos dojo finalizado en obra gruesa, quedan detalles
    git-svn-id: svn://saruman/admportal/trunk@227 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/components/controllers/DojoChartController.php
+++ application/modules/components/controllers/DojoChartController.php
@@ -29,35 +29,56 @@ class Components_DojoChartController extends Zend_Controller_Action
     
     public function init()
     {
-        $this->_form = new Zwei_Utils_Form();
         $this->_helper->layout()->disableLayout();
         
-        $this->_page = $this->_form->p;
+        $this->_page = $this->_request->getParam('p');
         $xml = new Zwei_Admin_Xml();
-        $file = Zwei_Admin_Xml::getFullPath($this->_page);
+        $file = $xml->getFullPath($this->_page);
         $xml->parse($file);
         $this->_layout = $xml->elements;
         
         $this->view->dojoStyle = $this->_dojo_style;
         $this->view->baseDojo = $this->_base_dojo_folder;
-        $this->view->title = isset($this->_layout[0]['NAME']) ? $this->_layout[0]['NAME'] : ""; 
-        $this->view->model = $this->_layout[0]['TARGET'];
-        $this->view->xTarget = $this->_layout[0]['CHART_X_TARGET'];
-        if (isset($this->_layout[0]['CHART_X_LABEL'])) $this->view->xTitle = $this->_layout[0]['CHART_X_LABEL'];
+        $this->view->title = isset($this->_layout[0]['NAME']) ? $this->_layout[0]['NAME'] : "";
+         
+        //Si existe $this->_layout[0]['TARGET'] se usa el modelo especificado en el XML, 
+        //pero se sigue usando el resto de los parametros
+        if (isset($this->_layout[0]['TARGET'])) {
+            $this->view->model = $this->_layout[0]['TARGET'];
+        }
+        
+        $uri = html_entity_decode(urldecode($this->_request->getParam('uri')));
+        if (!empty($uri)) { 
+            $aUri = parse_url($uri);
+            $aParams = array(); 
+            
+            parse_str($aUri['query'], $aParams);
+            
+            if (isset($this->_layout[0]['TARGET'])) $aParams['model'] = $this->_layout[0]['TARGET'];
+            if (isset($this->_layout[0]['GROUP_BY'])) $aParams['group'] = $this->_layout[0]['GROUP_BY'];
+    
+            if (!empty($aUri)) $this->view->url = $aUri['scheme'].'://'.$aUri['host'].$aUri['path'].'?'.str_replace('%3B', ';',http_build_query($aParams)); 
+        }
+        //Eje Y
+        $this->view->yTarget = $this->_layout[0]['CHART_Y_TARGET'];
+        if (isset($this->_layout[0]['CHART_Y_LABEL'])) $this->view->yTitle = $this->_layout[0]['CHART_Y_LABEL'];
         
         $this->view->options = isset($this->_layout[0]['OPTIONS']) ? $this->_layout[0]['OPTIONS'] : "new Object()";
-        $this->view->chartingTheme = isset($this->_layout[0]['CHART_THEME']) ? $this->_layout[0]['CHART_THEME'] : "Claro";
-        
-        
+        $this->view->chartingTheme = isset($this->_layout[0]['CHART_DOJO_THEME']) ? $this->_layout[0]['CHART_DOJO_THEME'] : "Claro";
         
         if (!empty($this->_request->style)) $this->_dojo_style = $this->_request->style;
-
     }
     
 
     public function indexAction()
     {
-        // action body
+        $this->view->xTarget = $this->_layout[0]['CHART_X_TARGET'];
+        $this->view->columns = $this->_layout[0]['CHART_COLUMNS'];
+        
+        
+        if (isset($this->_layout[0]['CHART_X_LABEL'])) $this->view->xTitle = $this->_layout[0]['CHART_X_LABEL'];
+        
+        $this->view->chartType = (isset($this->_layout[0]['CHART_DOJO_TYPE'])) ? $this->_layout[0]['CHART_DOJO_TYPE'] : "Lines";
     }
 
     public function pieAction()
@@ -67,14 +88,17 @@ class Components_DojoChartController extends Zend_Controller_Action
 
     public function barsAction()
     {
-        $this->view->yTarget = $this->_layout[0]['CHART_Y_TARGET'];
-        $this->view->yTitle = $this->_layout[0]['CHART_Y_LABEL'];
+        $this->view->xTarget = $this->_layout[0]['CHART_X_TARGET'];
+        if (isset($this->_layout[0]['CHART_X_LABEL'])) $this->view->xTitle = $this->_layout[0]['CHART_X_LABEL'];
     }
 
     public function linesAction()
     {
-        $this->view->yTarget = $this->_layout[0]['CHART_Y_TARGET'];
-        $this->view->yTitle = $this->_layout[0]['CHART_Y_LABEL'];
+        $this->view->xTarget = $this->_layout[0]['CHART_X_TARGET'];
+        $this->view->columns = $this->_layout[0]['CHART_COLUMNS'];
+        
+        
+        if (isset($this->_layout[0]['CHART_X_LABEL'])) $this->view->xTitle = $this->_layout[0]['CHART_X_LABEL'];
     }
 }
 
