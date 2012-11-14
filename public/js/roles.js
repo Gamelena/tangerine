585c574d9da588cc2e660c88979c02d67ba843bd
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-11-14 20:20:53 +0000

    #638 javascript redimensionar lightbox perfiles y permisos
    git-svn-id: svn://saruman/admportal/trunk@269 32ebc6c2-cfed-11df-be44-b108719c1499

--- public/js/roles.js
+++ public/js/roles.js
@@ -1,24 +1,30 @@
 function rolesInit()
 {
-	console.log('rolesInit');
-	//var formDialogo = dijit.byId('formDialogo'); // para explicacion, esto no es necesario ya existe var tabForm global (jsId)
-	require(["dojo/on"], function(on){
-		  on(formDialogo, "hide", function(e){
-			  dojo.query('.dijitDialog').style({'min-height':'416px',width:'424px'});
-		  });
-		  on(formDialogoEditar, "hide", function(e){
-			  dojo.query('.dijitDialog').style({'min-height':'416px',width:'424px'});
-		  });
-		  on(formDialogo, "show", function(e){
-			  setTimeout(function() {
-				  dojo.query('.dijitDialog').style({'min-height':null, height:'auto',width:'auto'});
-			  }, 2800);
-		  });
-		  on(formDialogoEditar, "show", function(e){
-			  setTimeout(function() {
-				  dojo.query('.dijitDialog').style({'min-height':null, height:'auto',width:'auto'});
-			  }, 2800);
-		  });
-		  
-	});
+    var domPrefix = (layout != undefined && layout == 'dijitTabs') ? 'roles_xml' : ''; 
+        
+    console.log('rolesInit');
+    //var formDialogo = dijit.byId('formDialogo'); // para explicacion, esto no es necesario ya existe var tabForm global (jsId)
+    require(["dojo/on"], function(on){
+        setTimeout(function() {
+            on(dijit.byId(domPrefix+'formDialogo'), "hide", function(e){
+                console.debug("formDialogo hide");
+                dojo.query('.dijitDialog').style({'min-height':'416px',width:'424px'});
+            });    
+              
+            on(dijit.byId(domPrefix+'formDialogoEditar'), "hide", function(e){
+                console.debug("formDialogoEditar hide");
+                dojo.query('.dijitDialog').style({'min-height':'416px',width:'424px'});
+            });
+              
+            on(dijit.byId(domPrefix+'formDialogo'), "show", function(e){
+                console.debug("formDialogo show");
+                dojo.query('.dijitDialog').style({'min-height':null, height:'auto',width:'auto'});
+            });
+              
+            on(dijit.byId(domPrefix+'formDialogoEditar'), "show", function(e){
+                console.debug("formDialogoEditar show");
+                dojo.query('.dijitDialog').style({'min-height':null, height:'auto',width:'auto'});
+            });
+        }, 800);  
+    });
 }
