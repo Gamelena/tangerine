b764b80ce4af00e684e07a98804268e2925d7747
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-11-16 21:59:18 +0000

    #638 se borran mensajes a log
    git-svn-id: svn://saruman/admportal/trunk@281 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Db/Table.php
+++ library/Zwei/Db/Table.php
@@ -156,7 +156,7 @@ class Zwei_Db_Table extends Zend_Db_Table_Abstract
             $strData = print_r($data, true);
             $logMessage = "[$userName $ip] INSERT INTO " . $this->info(Zend_Db_Table::NAME) . " VALUES ($strData) ";
             //Debug::writeBySettings($this->getAdapter()->getConfig(), 'transactions_log', 'SI', "../log/transactions");
-            //Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
+            Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
         }
 	    
 	    return parent::insert($data);
@@ -179,7 +179,7 @@ class Zwei_Db_Table extends Zend_Db_Table_Abstract
             $strWhere = print_r($where, true);
             $logMessage = "[$userName $ip] UPDATE " . $this->info(Zend_Db_Table::NAME) . " SET (".$strData.") WHERE (".$strWhere.") ";
             //Debug::writeBySettings($this->getAdapter()->getConfig(), 'transactions_log', 'SI', "../log/transactions");
-            //Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
+            Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
         }
 	    
 	    return parent::update($data, $where);
@@ -199,7 +199,7 @@ class Zwei_Db_Table extends Zend_Db_Table_Abstract
             $strWhere = print_r($where, true);
             $logMessage = "[$userName $ip] DELETE FROM " . $this->info(Zend_Db_Table::NAME) . " WHERE ($strWhere) ";
             //Debug::writeBySettings($this->getAdapter()->getConfig(), 'transactions_log', 'SI', "../log/transactions");
-            //Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
+            Debug::writeBySettings($logMessage, 'transactions_log', 'SI', "../log/transactions");
         }
 
         return parent::delete($where);
