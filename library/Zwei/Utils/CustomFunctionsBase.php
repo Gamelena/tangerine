b3b0f6a5954220ba84ed1f5f497863011e0e810d
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-12-07 21:30:52 +0000

    #638 Zwei_Utils_CustomFunctionsBase se cambian tabs por 4 espacios
    git-svn-id: svn://saruman/admportal/trunk@306 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Utils/CustomFunctionsBase.php
+++ library/Zwei/Utils/CustomFunctionsBase.php
@@ -17,98 +17,98 @@
 
 class Zwei_Utils_CustomFunctionsBase
 {
-	/**
-	 * Icono Dijit para pintar boton [TODO] diseñar implementar configuración de esto en XML.
-	 * @var array
-	 */
-	protected $_icons;
-	
-	/**
-	 * Id Capturada de la fila seleccionada de la grilla al llamar a la función .
-	 * @var mixed
-	 */
-	protected $_id;
-	
+    /**
+     * Icono Dijit para pintar boton [TODO] diseñar implementar configuración de esto en XML.
+     * @var array
+     */
+    protected $_icons;
+    
+    /**
+     * Id Capturada de la fila seleccionada de la grilla al llamar a la función .
+     * @var mixed
+     */
+    protected $_id;
+    
     /**
      * URL de busqueda original.
      * @var string
      */
-	
-	protected $_uri;
-	
-	
-	 /**
+    
+    protected $_uri;
+    
+    
+     /**
      * Parámetros de búsqueda.
      * @var array
      */
     
     protected $_query_params;
-	
-	/**
-	 * Permisos de usuario en sesión.
-	 * @var Zwei_Admin_Acl
-	 */
-	
-	protected $_acl;
-	
-	/**
-	 * Datos de sesión de usuario.
-	 * @var Zend_Auth_Storage
-	 */
-	protected $_user_info;
-	
-	
-	/**
-	 * Array asociativo entre el nombre del método y la descripción a mostrar, para pintar botones.
-	 * @var array
-	 */
-	protected $_names=array(
-		'clonarPromocion'=>'Clonar Promocion',
-		'procesarArchivoAbonados'=>'Procesar Archivo'
-	);
+    
+    /**
+     * Permisos de usuario en sesión.
+     * @var Zwei_Admin_Acl
+     */
+    
+    protected $_acl;
+    
+    /**
+     * Datos de sesión de usuario.
+     * @var Zend_Auth_Storage
+     */
+    protected $_user_info;
+    
+    
+    /**
+     * Array asociativo entre el nombre del método y la descripción a mostrar, para pintar botones.
+     * @var array
+     */
+    protected $_names=array(
+        'clonarPromocion'=>'Clonar Promocion',
+        'procesarArchivoAbonados'=>'Procesar Archivo'
+    );
 
-	/**
-	 * Objeto $_REQUEST.
-	 * @var Zwei_Utils_Form
-	 */
-	protected $_form;
-	
-	
-	public function __construct($icon=false)
-	{
-		if (!Zend_Auth::getInstance()->hasIdentity()) {
-		    Zend_Controller_Action_HelperBroker::getStaticHelper('redirector')->gotoAndExit('login');
-		}
-    	
-    	$this->_user_info = Zend_Auth::getInstance()->getStorage()->read();
+    /**
+     * Objeto $_REQUEST.
+     * @var Zwei_Utils_Form
+     */
+    protected $_form;
+    
+    
+    public function __construct($icon=false)
+    {
+        if (!Zend_Auth::getInstance()->hasIdentity()) {
+            Zend_Controller_Action_HelperBroker::getStaticHelper('redirector')->gotoAndExit('login');
+        }
+        
+        $this->_user_info = Zend_Auth::getInstance()->getStorage()->read();
         $this->_acl = new Zwei_Admin_Acl($this->_user_info->user_name);
         $this->_form = new Zwei_Utils_Form();
         if (!empty($_REQUEST['uri'])) {
             parse_str(urldecode($_REQUEST['uri']), $aParams);
             $subForm = new Zwei_Utils_Form($aParams);
             $this->initQueryParams($subForm);
-        }		
-	}
-	
-	public function setId($value)
-	{
-		$this->_id=$value;
-	}
-	
-	public function getName($index){
-		return $this->_names[$index];
-	}
-	
-	/**
-	 * 
-	 * @param $index string
-	 * @return string
-	 */
-	public function getIcon($index)
-	{
-		return !empty($this->_icons[$index]) ? $this->_icons[$index] : "dijitIconFunction";
-	}
-	
+        }        
+    }
+    
+    public function setId($value)
+    {
+        $this->_id=$value;
+    }
+    
+    public function getName($index){
+        return $this->_names[$index];
+    }
+    
+    /**
+     * 
+     * @param $index string
+     * @return string
+     */
+    public function getIcon($index)
+    {
+        return !empty($this->_icons[$index]) ? $this->_icons[$index] : "dijitIconFunction";
+    }
+    
    /**
      * Inicializa $this->_query_params con un array asociativo de parámetros de busqueda
      * 
@@ -150,81 +150,81 @@ class Zwei_Utils_CustomFunctionsBase
         Debug::write($this->_query_params);
         return $this->_query_params;
     }
-	
-	
-	
-	/**
-	 * A continuación se ponen ejemplos como apoyo, no son son usados de ninguna otra forma 
-	 * sólo son de referencia para construir la clase Zwei_Utils_CustomFunctions en cada proyecto   
-	 * 
-	 */
-	
-	
-	
-	/**
-	 * Ejemplo Clonar Promoción de Bonos Consumo 
-	 * @example
-	 * <code><component {...} functions="clonar_promocion_examples" functions_permissions="add" ...</code>
-	 */
-	public function clonarPromocionExample()
-	{
-		echo "
-		<script type=\"text/javascript\">
-			window.parent.cargarTabsPanelCentral('promociones', 'clone', 'id_promo');
-		</script>
-		";
-	}
-	
-	/**
-	 * Ejemplo Procesar Archivo abonados de Bonos Consumo 
+    
+    
+    
+    /**
+     * A continuación se ponen ejemplos como apoyo, no son son usados de ninguna otra forma 
+     * sólo son de referencia para construir la clase Zwei_Utils_CustomFunctions en cada proyecto   
+     * 
+     */
+    
+    
+    
+    /**
+     * Ejemplo Clonar Promoción de Bonos Consumo 
+     * @example
+     * <code><component {...} functions="clonar_promocion_examples" functions_permissions="add" ...</code>
+     */
+    public function clonarPromocionExample()
+    {
+        echo "
+        <script type=\"text/javascript\">
+            window.parent.cargarTabsPanelCentral('promociones', 'clone', 'id_promo');
+        </script>
+        ";
+    }
+    
+    /**
+     * Ejemplo Procesar Archivo abonados de Bonos Consumo 
      * @example
      * <code><componenent {...} functions="procesar_archivo_abonado_example" functions_permissions="add" ...</code>
-	 */
-	
-	public function procesarArchivoAbonadosExample($target)
-	{
-		chmod($target, 0777);
-		$new_path = "/home/promociones/files/processed";	 
-		$Abonados = new AbonadosModel();
-			 
-		$db = $Abonados->getAdapter();
+     */
+    
+    public function procesarArchivoAbonadosExample($target)
+    {
+        chmod($target, 0777);
+        $new_path = "/home/promociones/files/processed";     
+        $Abonados = new AbonadosModel();
+             
+        $db = $Abonados->getAdapter();
 
-		$db->query("DELETE FROM abonados_tmp");
-		$query = "LOAD DATA LOCAL INFILE '$target' IGNORE INTO TABLE abonados_tmp ( `msisdn` );";
-		Zwei_Utils_Debug::writeBySettings($query, 'query_log'); 
-	
-		if ($db->query($query)) {
-		 	try{
-			 	$query="INSERT IGNORE INTO abonados (msisdn, id_promocion) SELECT msisdn, $this->_id FROM abonados_tmp";
-			 	Zwei_Utils_Debug::writeBySettings($query, 'query_log');
-			 	if ($response=$db->query($query)) {
-			 		$inserted_rows=$response->rowCount();
-			 		
-			 		$where = array();
-					$where[] = $db->quoteInto('msisdn = ?', 0);
-					$where[] = $db->quoteInto('id_promocion = ?', $this->_id);
-					$Abonados->delete($where);
-							 		
-		 			$filename = substr($target, strrpos($target,'/')+1,strlen($target)-strrpos($target,'/'));
-		 			$new_target=$new_path."/".$filename;
-   					rename($target,$new_target);
-   					$row_file="row_".str_replace('.','', $filename);
-		 			echo "<script>alert('Archivo procesado correctamente, $inserted_rows registros insertados.');
-		 			window.parent.get_url_contents('ajax/abonados-count?id=".$this->_id."','ajax_box');
-		 			window.parent.document.getElementById('$row_file').style.display='none';
-		 			window.parent.document.getElementById('ajax_loading_bar').style.background='#FFF';
-		 			</script>";	
-			 	} else {
-					echo "<script>alert('Error en la carga de abonados.')</script>";				 		
-			 		
-			 	}
-		 	} catch(Zend_Db_Exception $e) {
-		 		echo "<script>alert('Error en la carga de abonados.')</script>";	
-		 		Zwei_Utils_Debug::write($e->getMessage().$e->getCode());
-		 	} 		
-	
-		} else {
-			echo "<script>alert('Error al procesar archivo.')</script>";		 		
-		}	
-	}	
+        $db->query("DELETE FROM abonados_tmp");
+        $query = "LOAD DATA LOCAL INFILE '$target' IGNORE INTO TABLE abonados_tmp ( `msisdn` );";
+        Zwei_Utils_Debug::writeBySettings($query, 'query_log'); 
+    
+        if ($db->query($query)) {
+             try{
+                 $query="INSERT IGNORE INTO abonados (msisdn, id_promocion) SELECT msisdn, $this->_id FROM abonados_tmp";
+                 Zwei_Utils_Debug::writeBySettings($query, 'query_log');
+                 if ($response=$db->query($query)) {
+                     $inserted_rows=$response->rowCount();
+                     
+                     $where = array();
+                    $where[] = $db->quoteInto('msisdn = ?', 0);
+                    $where[] = $db->quoteInto('id_promocion = ?', $this->_id);
+                    $Abonados->delete($where);
+                                     
+                     $filename = substr($target, strrpos($target,'/')+1,strlen($target)-strrpos($target,'/'));
+                     $new_target=$new_path."/".$filename;
+                       rename($target,$new_target);
+                       $row_file="row_".str_replace('.','', $filename);
+                     echo "<script>alert('Archivo procesado correctamente, $inserted_rows registros insertados.');
+                     window.parent.get_url_contents('ajax/abonados-count?id=".$this->_id."','ajax_box');
+                     window.parent.document.getElementById('$row_file').style.display='none';
+                     window.parent.document.getElementById('ajax_loading_bar').style.background='#FFF';
+                     </script>";    
+                 } else {
+                    echo "<script>alert('Error en la carga de abonados.')</script>";
+                     
+                 }
+             } catch(Zend_Db_Exception $e) {
+                 echo "<script>alert('Error en la carga de abonados.')</script>";
+                 Zwei_Utils_Debug::write($e->getMessage().$e->getCode());
+             }         
+    
+        } else {
+            echo "<script>alert('Error al procesar archivo.')</script>";
+        }    
+    }
 }
