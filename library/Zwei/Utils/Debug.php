9442918b7a6d8e38845c0fd4c5c958c5d9d337ea
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2012-12-07 21:24:27 +0000

    #638 Zwei_Utils_Debug se cambian tabs por 4 espacios
    git-svn-id: svn://saruman/admportal/trunk@304 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Utils/Debug.php
+++ library/Zwei/Utils/Debug.php
@@ -13,56 +13,56 @@
  */
 class Zwei_Utils_Debug
 {
-	
+    
   /**
    * Escribe el reporte de error en un archivo de texto plano llamado debug
    * @param string $message - texto a escribir en archivo. 
    * @param $file - ruta relativa del archivo a escribir.
-   */	
+   */    
     function write($message = null, $file = "" )
     {
         if( $file == "" ) $file = ROOT_DIR."/log/debug";
         $trace = debug_backtrace() ;
-	    if  ($message !== null ){
-	        $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').']: '.print_r($message, 1);
-	    } else {
-	        //$message = "[".strftime('%Y-%m-%d %H:%M:%S')."]\n".print_r($trace, 1);
-	        $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').'] El grillo dijo "cri cri" (acá no hay nada).';
-	    }
-	    $ff = fopen($file, "a");
-	    fwrite($ff,"$message\r\n");
-	    fclose($ff);
-	}
-	
-	/**
-	 * Escribe mensaje en archivo de log, dependiendo de valores de tabla de configuración global (web_settings). 
-	 *  
-	 * @param string $message - texto a escribir en archivo.
-	 * @param string $settings_id - PK de web_settings.
-	 * @param string $settings_value - valor de 'value' esperado de web_settings para escribir en archivo. 
-	 * @param string $file - ruta relativa del archivo a escribir.
-	 * @return void
-	 */
-	function writeBySettings($message, $settings_id, $settings_value='SI', $file= "../log/debug")
-	{
+        if  ($message !== null ){
+            $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').']: '.print_r($message, 1);
+        } else {
+            //$message = "[".strftime('%Y-%m-%d %H:%M:%S')."]\n".print_r($trace, 1);
+            $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').'] El grillo dijo "cri cri" (acá no hay nada).';
+        }
+        $ff = fopen($file, "a");
+        fwrite($ff,"$message\r\n");
+        fclose($ff);
+    }
+    
+    /**
+     * Escribe mensaje en archivo de log, dependiendo de valores de tabla de configuración global (web_settings). 
+     *  
+     * @param string $message - texto a escribir en archivo.
+     * @param string $settings_id - PK de web_settings.
+     * @param string $settings_value - valor de 'value' esperado de web_settings para escribir en archivo. 
+     * @param string $file - ruta relativa del archivo a escribir.
+     * @return void
+     */
+    function writeBySettings($message, $settings_id, $settings_value='SI', $file= "../log/debug")
+    {
         $oSettings = new SettingsModel();
         try {
             $oSettingsSelect = $oSettings->select()->where('id = ?', $settings_id);
             $aSettings = $oSettings->fetchRow($oSettingsSelect);
             if (isset($aSettings) && $aSettings->value == $settings_value) {
                 $trace = debug_backtrace();
-            	if ($message !==null ) {
-	               $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').']: '.print_r($message, 1);
-		        } else {
-		           //$message = "[".strftime('%Y-%m-%d %H:%M:%S')."]\n".print_r($trace, 1);
-		           $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').'] El grillo dijo "cri cri" (acá no hay nada).';
-		        }
-		        $ff = fopen($file, "a");
-		        fwrite($ff, "$message\r\n");
-		        fclose($ff);
+                if ($message !==null ) {
+                   $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').']: '.print_r($message, 1);
+                } else {
+                   //$message = "[".strftime('%Y-%m-%d %H:%M:%S')."]\n".print_r($trace, 1);
+                   $message = $trace[0]['file'].'['.$trace[0]['line'].']['.strftime('%Y-%m-%d %H:%M:%S').'] El grillo dijo "cri cri" (acá no hay nada).';
+                }
+                $ff = fopen($file, "a");
+                fwrite($ff, "$message\r\n");
+                fclose($ff);
             }
         } catch (Zend_Db_Exception $e) {
             Zwei_Utils_Debug::write("Error {$e->getCode()} {$e->getMessage()}");
         } 
-	}	
+    }    
 }
