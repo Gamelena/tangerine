9c57b0bbc581014401c5eeadf16e5a8c0b01ac64
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2013-09-02 20:03:03 +0000

    #817 creacion de input binary dojox-form-checked-multiselect-binary para campos con mascaras binarias
    git-svn-id: svn://saruman/admportal/trunk@472 32ebc6c2-cfed-11df-be44-b108719c1499

---
+++ library/Zwei/Admin/ModelInterface.php
@@ -0,0 +1,26 @@
+<?php 
+interface Zwei_Admin_ModelInterface
+{
+    const PRIMARY          = 'primary';
+    
+    public function select();
+    
+    public function fetchAll();
+    
+    public function fetchRow();
+    /**
+     * Returns table information.
+     *
+     * You can elect to return only a part of this information by supplying its key name,
+     * otherwise all information is returned as an array.
+     *
+     * @param  string $key The specific info part to return OPTIONAL
+     * @return mixed
+     * @throws Zend_Db_Table_Exception
+     */
+    public function info($key=null);
+    /**
+     * @return string
+     */
+    public function __toString();
+}
