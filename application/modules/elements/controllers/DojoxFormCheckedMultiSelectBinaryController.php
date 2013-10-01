e98a59833f687e865e8a3d26027b3dea4200e620
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2013-10-01 17:53:01 +0000

    #817 dojox.form.CheckedMultiSelect soporta estado disabled
    git-svn-id: svn://saruman/admportal/trunk@502 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/elements/controllers/DojoxFormCheckedMultiSelectBinaryController.php
+++ application/modules/elements/controllers/DojoxFormCheckedMultiSelectBinaryController.php
@@ -28,8 +28,9 @@ class Elements_DojoxFormCheckedMultiSelectBinaryController extends Zend_Controll
         $this->view->domId =  $r->getParam('domId');
         $this->view->target =  $r->getParam('target');
         
-        $this->view->readonly = $r->getParam('readonly', '') === 'true' ? "readonly=\"readonly\"" : '';
-        $this->view->disabled = $r->getParam('disabled', '') === 'true' ? "disabled=\"disabled\"" : '';
+        $this->view->readonly = $r->getParam('readonly') === 'true' || $r->getParam($r->getParam('mode')) == 'readonly' ? "readonly=\"readonly\"" : '';
+        $this->view->disabled = $r->getParam('disabled') === 'true' || $r->getParam($r->getParam('mode')) == 'disabled' ? "disabled=\"disabled\"" : '';
+        
         $this->view->required = $r->getParam('required', '') === 'true' ? "required=\"true\"" : '';
         $this->view->onblur = $r->getParam('onblur') ? "onblur=\"{$r->getParam('onblur')}\"" : '';
         $this->view->style = $r->getParam('style') ? $r->getParam('style') : '';
