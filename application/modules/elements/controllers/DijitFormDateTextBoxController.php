d36c5734ce1ccc8b3dd92d48c700f0895a328339
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2013-12-23 18:27:33 +0000

    #817 soporte onchange para dijit.form.DateTextBox
    git-svn-id: svn://saruman/admportal/trunk@617 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/elements/controllers/DijitFormDateTextBoxController.php
+++ application/modules/elements/controllers/DijitFormDateTextBoxController.php
@@ -19,6 +19,7 @@ class Elements_DijitFormDateTextBoxController extends Zend_Controller_Action
         $this->view->disabled = $r->getParam('disabled') === 'true' || $r->getParam($r->getParam('mode')) == 'disabled' ? "disabled=\"disabled\"" : '';
         $this->view->required = $r->getParam('required', '') === 'true' ? "required=\"true\"" : '';
         $this->view->onblur = $r->getParam('onblur') ? "onblur=\"{$r->getParam('onblur')}\"" : '';
+        $this->view->onchange = $r->getParam('onchange') ? "onchange=\"{$r->getParam('onchange')}\"" : '';
         $this->view->regExp = $r->getParam('regExp') ? "regExp=\"{$r->getParam('regExp')}\"" : '';
         $this->view->invalidMessage = $r->getParam('invalidMessage') ? "invalidMessage=\"{$r->getParam('invalidMessage')}\"" : '';
         $this->view->promptMessage= $r->getParam('promptMessage') ? "promptMessage=\"{$r->getParam('promptMessage')}\"" : '';
