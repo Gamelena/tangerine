80ca34cac5cf48b449144f3af3faf251c7f9c6b1
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-01-08 16:26:32 +0000

    #817 se omite funcionalidad extra sin terminar en listener de eventos
    git-svn-id: svn://saruman/admportal/trunk@635 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/controllers/EventsController.php
+++ application/controllers/EventsController.php
@@ -10,8 +10,35 @@ class EventsController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        if (!Zwei_Admin_Auth::getInstance()->hasIdentity()) {
-                echo "<script>window.parent.location.href='".BASE_URL."/admin'</script>";
+        if (!Zwei_Admin_Auth::getInstance()->hasIdentity())
+        {
+            echo "<script>window.parent.location.href='".BASE_URL."/admin';</script>";
+        } else {
+            $auth = Zend_Auth::getInstance();
+            $authInfo = $auth->getStorage()->read();
+            $aclRoles = new AclRolesModel();
+            
+            
+            if (false) {
+                $authAdapter = Zwei_Admin_Auth::getInstance()->getAuthAdapter(false);
+                $userModel = new DbTable_AclUsers();
+                $currentUser = $userModel->find($authInfo->id)->current();
+                $username = $currentUser->user_name;
+                $password = $currentUser->password;
+                
+                $authAdapter->setIdentity($username)
+                ->setCredential($password);
+
+                $result = $auth->authenticate($authAdapter);
+                
+                if($result->isValid())
+                {
+                    Zwei_Admin_Auth::initUserInfo($authAdapter);
+                    $acl=new Zwei_Admin_Acl();
+                } else {
+                    echo "<script>window.parent.location.href='".BASE_URL."/admin';</script>";
+                }
+            }
         }
     }
 }
