36c71e1602b4e2eaac5cc3b120326b90c158cc75
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-01-08 18:03:31 +0000

    #817 se elimina refresco de sesion activa en EventsController, ya que al usar una conexion "persistente" provocaba que la sesion jamas expirara
    git-svn-id: svn://saruman/admportal/trunk@636 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Controller/Plugin/TimeOutHandler.php
+++ library/Zwei/Controller/Plugin/TimeOutHandler.php
@@ -8,11 +8,12 @@ class Zwei_Controller_Plugin_TimeOutHandler extends Zend_Controller_Plugin_Abstr
 {
     public function preDispatch(Zend_Controller_Request_Abstract $request) {
         $authNamespace = new Zend_Session_Namespace('Zend_Auth');
+        
         // clear the identity of a user who has not accessed a controller for
         // longer than a timeout period.
         if (isset($authNamespace->timeout) && time() > $authNamespace->timeout && $request->getActionName() != "login") {
             Zend_Auth::getInstance()->clearIdentity();
-        } else {
+        } else if ($request->getControllerName() != 'events' || $request->getModuleName() != 'default') {
             // User is still active - update the timeout time.
             $authNamespace->timeout = time() + 3600;
             // Store the request URI so that an authentication after a timeout
