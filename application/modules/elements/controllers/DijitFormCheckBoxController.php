96e0063ce425852ded68b0bd48194626ab4f83bd
paulina <paulina@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-01-20 20:02:47 +0000

    #817 fix en checkbox disabled
    git-svn-id: svn://saruman/admportal/trunk@658 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/elements/controllers/DijitFormCheckBoxController.php
+++ application/modules/elements/controllers/DijitFormCheckBoxController.php
@@ -19,7 +19,8 @@ class Elements_DijitFormCheckBoxController extends Zend_Controller_Action
         $this->view->uncheckedValue = $r->getParam('uncheckedValue', '0');
         $this->view->checked = $r->getParam('value') == $this->view->defaultValue ? "checked" : "";
         if ($r->getParam('checked')) $this->view->checked = "checked";
-        $this->view->disabled = $r->getParam('disabled', '') === 'true' ? "disabled=\"disabled\"" : '';
+        $this->view->readonly = $r->getParam('readonly') === 'true' || $r->getParam($r->getParam('mode')) == 'readonly' ? "readonly=\"readonly\"" : '';
+        $this->view->disabled = $r->getParam('disabled') === 'true' || $r->getParam($r->getParam('mode')) == 'disabled' ? "disabled=\"disabled\"" : '';
         $this->view->onchange = $r->getParam('onchange', '');
         $this->view->onclick = $r->getParam('onclick') ? "onclick=\"{$r->getParam('onclick')}\"" : '';
     }
