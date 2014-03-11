b8eef6f47d0a7bd1540716880a1d87aa494b4b87
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-03-11 15:18:31 +0000

    #915 script de actualizacion segura de BD
    git-svn-id: svn://saruman/admportal/trunk@707 32ebc6c2-cfed-11df-be44-b108719c1499

--- db/updates/update-817.sql
+++ db/updates/update-817.sql
@@ -1,3 +1,70 @@
+-- Reemplazar por nombre de base de datos de admportal para gw ussd 
+USE USSD;
+
+DROP PROCEDURE IF EXISTS changeColumnUnlessExists;
+
+DELIMITER ;;
+
+CREATE PROCEDURE changeColumnUnlessExists(
+    IN dbName tinytext,
+    IN tableName tinytext,
+    IN oldFieldName tinytext,
+    IN newFieldName tinytext,
+    IN newFieldDef text)
+BEGIN
+    IF NOT EXISTS (
+        SELECT * FROM information_schema.COLUMNS
+        WHERE column_name=newFieldName
+        and table_name=tableName
+        and table_schema=dbName
+        )
+    THEN
+        set @ddl=CONCAT('ALTER TABLE `', dbName, '`.`', tableName,
+            '` CHANGE `', oldFieldName, '` `', newFieldName ,'` ', newFieldDef);
+        prepare stmt from @ddl;
+        execute stmt;
+    END IF;
+END;
+;;
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS addColumnUnlessExists;
+DELIMITER ;;
+
+
+CREATE PROCEDURE addColumnUnlessExists(
+    IN dbName tinytext,
+    IN tableName tinytext,
+    IN fieldName tinytext,
+    IN fieldDef text)
+BEGIN
+    IF NOT EXISTS (
+        SELECT * FROM information_schema.COLUMNS
+        WHERE column_name=fieldName
+        and table_name=tableName
+        and table_schema=dbName
+        )
+    THEN
+        set @ddl=CONCAT('ALTER TABLE `',dbName,'`.`',tableName,
+            '` ADD COLUMN `',fieldName,'` ',fieldDef);
+        prepare stmt from @ddl;
+        execute stmt;
+    END IF;
+END;
+;;
+DELIMITER ;
+
+
+DELETE from acl_modules_actions WHERE acl_modules_id NOT IN (SELECT id FROM acl_modules);
+DELETE from acl_modules_actions WHERE acl_actions_id NOT IN (SELECT id FROM acl_actions);
+DELETE from acl_roles_modules_actions WHERE acl_roles_id NOT IN (SELECT id FROM acl_roles);
+DELETE from acl_roles_modules_actions WHERE acl_modules_actions_id NOT IN (SELECT id FROM acl_modules_actions);
+
+
+CALL addColumnUnlessExists(Database(), 'acl_modules', 'ownership', 'enum("0","1") NOT NULL DEFAULT "0" COMMENT "Indica si modulo usa modelo con reglas de owner, funciona con type=xml, ver Zwei_Db_Table::isOwner(item, user)"');
+CALL addColumnUnlessExists(Database(), 'acl_roles', 'must_refresh', 'enum("0","1") NOT NULL DEFAULT "0"');
+CALL addColumnUnlessExists(Database(), 'acl_users', 'must_refresh', 'enum("0","1") NOT NULL DEFAULT "0"');
+
 DROP TABLE IF EXISTS `acl_session`;
 
 CREATE TABLE IF NOT EXISTS `acl_session` (
@@ -11,6 +78,3 @@ CREATE TABLE IF NOT EXISTS `acl_session` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DELETE FROM acl_roles_modules_actions WHERE acl_roles_id NOT IN (SELECT id FROM acl_roles);
-DELETE FROM acl_modules_actions WHERE acl_modules_id NOT IN (SELECT id FROM acl_modules);
-ALTER TABLE acl_modules ADD `ownership` enum('0','1') NOT NULL DEFAULT '0' COMMENT 'Indica si modulo usa modelo con reglas de owner, funciona con type=xml, ver Zwei_Db_Table::isOwner(item, user)';
