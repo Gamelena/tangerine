f9ece07947af6b46a3578edcf04e2d6dbcfacdea
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-04-16 13:04:02 +0000

    #817 fix typo zwei.form.ValidationTextarea
    git-svn-id: svn://saruman/admportal/trunk@763 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -94,7 +94,7 @@ class AdminController extends Zend_Controller_Action
         ->requireModule("zwei.Utils")
         ->requireModule("zwei.Admportal")
         ->requireModule("zwei.Form")
-        ->requireModule("zwei.form.ValidationTexarea")
+        ->requireModule("zwei.form.ValidationTextarea")
         ->requireModule("dojox.grid.enhanced.plugins.Pagination")
         ;
     
