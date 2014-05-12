c8d9153fd325c71c761f9187d0aeb8944fe5f891
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-05-12 19:27:56 +0000

    #817 script sql para forzar integridad referencial, borra acl_modules.parent_id huerfanos
    git-svn-id: svn://saruman/admportal/trunk@792 32ebc6c2-cfed-11df-be44-b108719c1499

--- db/forzar-integridad-referencial.sql
+++ db/forzar-integridad-referencial.sql
@@ -1,4 +1,5 @@
 DELETE from acl_modules_actions WHERE acl_modules_id NOT IN (SELECT id FROM acl_modules);
+DELETE from acl_modules_actions WHERE acl_modules_id NOT IN (NULL, (SELECT m1.parent_id FROM acl_modules m1 LEFT JOIN acl_modules m2 ON m1.parent_id = m2.id WHERE m1.parent_id != NULL ));
 DELETE from acl_modules_actions WHERE acl_actions_id NOT IN (SELECT id FROM acl_actions);
 DELETE from acl_roles_modules_actions WHERE acl_roles_id NOT IN (SELECT id FROM acl_roles);
 DELETE from acl_roles_modules_actions WHERE acl_modules_actions_id NOT IN (SELECT id FROM acl_modules_actions);
