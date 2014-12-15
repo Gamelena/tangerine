c9e2fd58f03f7106ccc6523d389023b9eeec9fa3
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2014-12-15 12:59:02 +0000

    #817 fixes en inputs dijit.form.ValidationTextBox, dojo.dnd.source, dojox.form.CheckedMultiSelect
    git-svn-id: svn://saruman/admportal/trunk@930 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/elements/controllers/DijitFormValidationTextBoxController.php
+++ application/modules/elements/controllers/DijitFormValidationTextBoxController.php
@@ -17,20 +17,20 @@ class Elements_DijitFormValidationTextBoxController extends Zend_Controller_Acti
         
         $this->view->value =  $r->getParam('value', $r->getParam('defaultValue', ''));
         $this->view->type = $r->getParam('password') === 'true' ? 'password' : 'text' ;
-        $this->view->readonly = $r->getParam('readonly') === 'true' || $r->getParam($r->getParam('mode')) == 'readonly' ? "readonly=\"readonly\"" : '';
-        $this->view->disabled = $r->getParam('disabled') === 'true' || $r->getParam($r->getParam('mode')) == 'disabled' ? "disabled=\"disabled\"" : '';
-        $this->view->required = $r->getParam('required') === 'true' ? "required=\"true\"" : '';
-        $this->view->onblur = $r->getParam('onblur') ? "onblur=\"{$r->getParam('onblur')}\"" : '';
-        $this->view->style = $r->getParam('onblur') ? "style=\"{$r->getParam('style')}\"" : '';
-        $this->view->regExp = $r->getParam('regExp') ? "pattern=\"{$r->getParam('regExp')}\"" : '';
-        $this->view->onchange = $r->getParam('onchange') ? "onchange=\"{$r->getParam('onchange')}\"" : '';
-        $this->view->onclick = $r->getParam('onclick') ? "onclick=\"{$r->getParam('onclick')}\"" : '';
-        $this->view->placeHolder = $r->getParam('placeHolder') ? "placeHolder=\"{$r->getParam('placeHolder')}\"" : '';
-        $this->view->invalidMessage = $r->getParam('invalidMessage') ? "invalidMessage=\"{$r->getParam('invalidMessage')}\"" : '';
-        $this->view->promptMessage= $r->getParam('promptMessage') ? "promptMessage=\"{$r->getParam('promptMessage')}\"" : '';
-        $this->view->autocomplete= $r->getParam('autocomplete') ? "autocomplete=\"{$r->getParam('autocomplete')}\"" : '';
-        $this->view->maxlength = $r->getParam('maxlength')? "maxLength=\"{$r->getParam('maxlength')}\"" : '';
-        $this->view->trim = $r->getParam('trim', '') === 'true' ? "trim=\"true\"" : '';
+        $this->view->readonly = $r->getParam('readonly') === 'true' || $r->getParam($r->getParam('mode')) == 'readonly' ? " readonly=\"readonly\"" : '';
+        $this->view->disabled = $r->getParam('disabled') === 'true' || $r->getParam($r->getParam('mode')) == 'disabled' ? " disabled=\"disabled\"" : '';
+        $this->view->required = $r->getParam('required') === 'true' ? " required=\"true\"" : '';
+        $this->view->onblur = $r->getParam('onblur') ? " onblur=\"{$r->getParam('onblur')}\"" : '';
+        $this->view->style = $r->getParam('style') ? " style=\"{$r->getParam('style')}\"" : '';
+        $this->view->regExp = $r->getParam('regExp') ? " pattern=\"{$r->getParam('regExp')}\"" : '';
+        $this->view->onchange = $r->getParam('onchange') ? " onchange=\"{$r->getParam('onchange')}\"" : '';
+        $this->view->onclick = $r->getParam('onclick') ? " onclick=\"{$r->getParam('onclick')}\"" : '';
+        $this->view->placeHolder = $r->getParam('placeHolder') ? " placeHolder=\"{$r->getParam('placeHolder')}\"" : '';
+        $this->view->invalidMessage = $r->getParam('invalidMessage') ? " invalidMessage=\"{$r->getParam('invalidMessage')}\"" : '';
+        $this->view->promptMessage= $r->getParam('promptMessage') ? " promptMessage=\"{$r->getParam('promptMessage')}\"" : '';
+        $this->view->autocomplete= $r->getParam('autocomplete') ? " autocomplete=\"{$r->getParam('autocomplete')}\"" : '';
+        $this->view->maxlength = $r->getParam('maxlength')? " maxLength=\"{$r->getParam('maxlength')}\"" : '';
+        $this->view->trim = $r->getParam('trim', '') === 'true' ? " trim=\"true\"" : '';
     }
 }
 
