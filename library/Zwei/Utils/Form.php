02110028bf3399b15400c2f16416cd83f46bf961
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-01-12 13:28:24 +0000

    #817 comentarios, css e imagenes
    git-svn-id: svn://saruman/admportal/trunk@957 32ebc6c2-cfed-11df-be44-b108719c1499

--- library/Zwei/Utils/Form.php
+++ library/Zwei/Utils/Form.php
@@ -1,9 +1,17 @@
 <?php
 /**
- * Manejador de Requests. 
+ * Manejador de Requests.
  * 
  * Encapsula los $_POST, $_GET y $_FILES, codifica/decodifica los datos ingresados como html_entities UTF-8.
  * Los valores de los resultados son datos HTML.
+ * 
+ * Es aconsejable usar esta clase para manejar los $_GET y $_POST fuera de controladoes.
+ * 
+ * Para controladores Zend_Controller usar es aconsejable $this->getRequest() para los $_GET y $_POST en lugar de Zwei_Utils_Form, usar sólo para upload de archivos,
+ * ya que Zend_Controller::getRequest() ofrece mejor compatibilidad con Zend_Test_PHPUnit_ControllerTestCase.
+ * 
+ * 
+ *  
  * 
  * @package Zwei_Utils
  * @version $Id:$
