738f726d2e233d2181fe46eb76294ccfac5292fd
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-01-27 12:58:11 +0000

    #817 fix json de graficos servidos en puerto diferente al 80
    git-svn-id: svn://saruman/admportal/trunk@963 32ebc6c2-cfed-11df-be44-b108719c1499

--- application/modules/components/controllers/DojoChartController.php
+++ application/modules/components/controllers/DojoChartController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Controlador para generar gráficos con Dojo Toolkit.
  *
@@ -22,34 +23,33 @@
  *
  */
 
-
 /**
  * Controlador para generar gráficos con Dojo Toolkit.
  *
- * @example
- * <code>
- * <?xml version="1.0"?>
- * <!DOCTYPE section PUBLIC "//COMPONENTS/" "components.dtd">
- * <component 
- * name="Gráfico por Canal"
- * type="dojo-chart" 
- * chartYTarget="count"
- * chartXType="datetime"
- * chartXTarget="fecha"  
- * chartDojoType="Markers"
- * chartDojoTheme="Shrooms"
- * target="counter_c_d_r_compras"
- * />
- * </code>
- *
- *
- *
+ * @example <code>
+ *          <?xml version="1.0"?>
+ *          <!DOCTYPE section PUBLIC "//COMPONENTS/" "components.dtd">
+ *          <component
+ *          name="Gráfico por Canal"
+ *          type="dojo-chart"
+ *          chartYTarget="count"
+ *          chartXType="datetime"
+ *          chartXTarget="fecha"
+ *          chartDojoType="Markers"
+ *          chartDojoTheme="Shrooms"
+ *          target="counter_c_d_r_compras"
+ *          />
+ *          </code>
+ *         
+ *         
+ *         
  */
 class Components_DojoChartController extends Zend_Controller_Action
 {
 
     /**
      * Dojo theme
+     * 
      * @var string
      *
      */
@@ -57,13 +57,15 @@ class Components_DojoChartController extends Zend_Controller_Action
 
     /**
      * carpeta base de dojo toolkit
-     * @var string 
+     * 
+     * @var string
      *
      */
     private $_base_dojo_folder = '/dojotoolkit';
 
     /**
      * Nombre archivo xml
+     * 
      * @var string
      *
      */
@@ -71,12 +73,13 @@ class Components_DojoChartController extends Zend_Controller_Action
 
     /**
      * Objeto XML
+     * 
      * @var Zwei_Admin_Xml
      *
      */
     private $_xml = null;
 
-    public function init()
+    public function init ()
     {
         $this->_helper->layout()->disableLayout();
         
@@ -89,53 +92,68 @@ class Components_DojoChartController extends Zend_Controller_Action
         $this->view->dojoStyle = $this->_dojo_style;
         $this->view->baseDojo = $this->_base_dojo_folder;
         $this->view->title = $this->_xml->getAttribute("name") ? $this->_xml->getAttribute("name") : "";
-         
-        //Si existe $this->_layout[0]['TARGET'] se usa el modelo especificado en el XML, 
-        //pero se sigue usando el resto de los parametros
+        
+        // Si existe $this->_layout[0]['TARGET'] se usa el modelo especificado
+        // en el XML,
+        // pero se sigue usando el resto de los parametros
         if ($this->_xml->getAttribute("target")) {
             $this->view->model = $this->_xml->getAttribute("target");
         }
         
         $uri = html_entity_decode(urldecode($this->_request->getParam('uri')));
-        if (!empty($uri)) { 
+        $port = $_SERVER['SERVER_PORT'] != '80' ? '' . $_SERVER['SERVER_PORT'] : '';
+        if (! empty($uri)) {
             $aUri = parse_url($uri);
-            if (!empty($aUri)) {
-                $aParams = array(); 
+            if (! empty($aUri)) {
+                $aParams = array();
                 
                 parse_str($aUri['query'], $aParams);
                 
-                if ($this->_xml->getAttribute("target")) { $aParams['model'] = $this->_xml->getAttribute("target");}
-                else if ($this->_request->getParam("target")) { $aParams['model'] = $this->_request->getParam("target");}
+                if ($this->_xml->getAttribute("target")) {
+                    $aParams['model'] = $this->_xml->getAttribute("target");
+                } else {
+                    if ($this->_request->getParam("target")) {
+                        $aParams['model'] = $this->_request->getParam("target");
+                    }
+                }
                 
-                if ($this->_xml->getAttribute("group_by")) { $aParams['group'] = $this->_xml->getAttribute("group_by");}
-                else if ($this->_request->getParam("group_by")) { $aParams['group'] = $this->_request->getParam("group_by");}
-                
+                if ($this->_xml->getAttribute("group_by")) {
+                    $aParams['group'] = $this->_xml->getAttribute("group_by");
+                } else {
+                    if ($this->_request->getParam("group_by")) {
+                        $aParams['group'] = $this->_request->getParam("group_by");
+                    }
+                }
                 $aParams['p'] = $this->_request->getParam('p');
                 
                 $httpParams = http_build_query($aParams);
-                $this->view->url = $aUri['scheme'].'://'.$aUri['host'].$aUri['path'].'?'.str_replace('%3B', ';', $httpParams);
-            } 
+                $this->view->url = $aUri['scheme'] . ":$port//" . $aUri['host'] .
+                         $aUri['path'] . '?' .
+                         str_replace('%3B', ';', $httpParams);
+            }
         }
         $aParams['format'] = 'excel';
         $aParams['excel_formatter'] = 'csv';
         $this->view->excelParams = http_build_query($aParams);
         
-        //Eje Y
+        // Eje Y
         $this->view->yTarget = $this->_xml->getAttribute("chartYTarget");
-        if ($this->_xml->getAttribute("chart_y_label")) $this->view->yTitle = $this->_xml->getAttribute("chart_y_label");
+        if ($this->_xml->getAttribute("chart_y_label"))
+            $this->view->yTitle = $this->_xml->getAttribute("chart_y_label");
         
         $this->view->options = $this->_xml->getAttribute("options") ? $this->_xml->getAttribute("options") : "new Object()";
         $this->view->chartingTheme = $this->_xml->getAttribute("chartDojoTheme") ? $this->_xml->getAttribute("chartDojoTheme") : "Claro";
         
-        if (!empty($this->_request->style)) $this->_dojo_style = $this->_request->style;
+        if (! empty($this->_request->style))
+            $this->_dojo_style = $this->_request->style;
     }
 
-    public function indexAction()
+    public function indexAction ()
     {
         $this->view->xTarget = $this->_xml->getAttribute("chartXTarget");
         $this->view->items = $this->_xml->getAttribute("chart_items");
         $this->view->excel = $this->_xml->helpers && $this->_xml->helpers->excel;
-
+        
         if ($this->_request->getParam("chart_items")) {
             $this->view->items = $this->_request->getParam("chart_items");
             $this->view->url .= "&chart_items=" . $this->_request->getParam("chart_items");
@@ -143,31 +161,30 @@ class Components_DojoChartController extends Zend_Controller_Action
         
         if ($this->_request->getParam("chart_items_description")) {
             $this->view->items = $this->_request->getParam("chart_items_description");
-            $this->view->url .= "&chart_items_description=" . $this->_request->getParam("chart_items_description");            
+            $this->view->url .= "&chart_items_description=" . $this->_request->getParam("chart_items_description");
         }
         
+        if ($this->_xml->getAttribute("chart_x_label"))
+            $this->view->xTitle = $this->_xml->getAttribute("chart_x_label");
         
-        if ($this->_xml->getAttribute("chart_x_label")) $this->view->xTitle = $this->_xml->getAttribute("chart_x_label");
-        
-        $this->view->chartType = $this->_xml->getAttribute("chartDojoType") ?  $this->_xml->getAttribute("chartDojoType") : "Lines";
+        $this->view->chartType = $this->_xml->getAttribute("chartDojoType") ? $this->_xml->getAttribute(
+                "chartDojoType") : "Lines";
     }
 
-    public function pieAction()
-    {
-    }
+    public function pieAction ()
+    {}
 
-    public function barsAction()
-    {
-    }
+    public function barsAction ()
+    {}
 
-    public function linesAction()
-    {
-    }
+    public function linesAction ()
+    {}
 
-    public function excelAction()
+    public function excelAction ()
     {
         /**
-         * @var $model Zwei_Db_Table 
+         *
+         * @var $model Zwei_Db_Table
          */
         $model = new $this->view->model();
         
@@ -177,12 +194,13 @@ class Components_DojoChartController extends Zend_Controller_Action
         header("Content-Type: application/force-download");
         header("Content-Type: application/octet-stream");
         header("Content-Type: application/download");
-        header("Content-Disposition: attachment; filename={$this->view->model}.csv");
+        header(
+                "Content-Disposition: attachment; filename={$this->view->model}.csv");
         header('Content-Encoding: UTF-8');
         header('Content-type: text/csv; charset=UCS-2LE');
         
-        $elements = $this->_xml->elements ? $this->_xml->xpath('//component/elements/element') : array();
-        
+        $elements = $this->_xml->elements ? $this->_xml->xpath(
+                '//component/elements/element') : array();
         
         foreach ($elements as $element) {
             $labels[] = $element->getAttribute("name") ? $element->getAttribute("name") : $element->getAttribute("target");
@@ -192,7 +210,6 @@ class Components_DojoChartController extends Zend_Controller_Action
             $labels = false;
         }
         
-        
         $select = $model->select();
         $dbObject = new Zwei_Db_Object($this->getRequest()->getParams());
         $select = $dbObject->select();
@@ -201,11 +218,10 @@ class Components_DojoChartController extends Zend_Controller_Action
         $table = new Zwei_Utils_Table();
         
         $content = $table->rowsetToCsv($collection, $labels);
-        $this->view->content = chr(255) . chr(254) . mb_convert_encoding($content, 'UCS-2LE', 'UTF-8');
+        $this->view->content = chr(255) . chr(254) .
+                 mb_convert_encoding($content, 'UCS-2LE', 'UTF-8');
         $this->render();
     }
-
-
 }
 
 
