c439ef8089a8cf69ea7f7c9e14c576dc44a13c7d
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-03-12 17:58:14 +0000

    #817 generacion de tests unitarios base con admportal-create
    git-svn-id: svn://saruman/admportal/trunk@1000 32ebc6c2-cfed-11df-be44-b108719c1499

--- tests/application/controllers/AclControllerTest.php
+++ tests/application/controllers/AclControllerTest.php
@@ -5,12 +5,39 @@ class AclControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->bootstrap = $this->bootstrap = new Zend_Application(
+            APPLICATION_ENV,
+            $_ENV['APPLICATION_CONFIG']
+        );
+        $options = $this->bootstrap->getBootstrap()->getOptions();
+        $_SERVER['HTTP_HOST'] = $options['zwei']['uTesting']['httpHost'];
         parent::setUp();
     }
-
+    
+    public function initUserInfo()
+    {
+        $this->getFrontController()->setParam('bootstrap', $this->bootstrap->getBootstrap());
+        $authAdapter = Zwei_Admin_Auth::getInstance()->getAuthAdapter();
+    
+        $username = PHPUNIT_USERNAME;
+        $password = PHPUNIT_PASSWORD;
+        $auth = Zend_Auth::getInstance();
+    
+        $authAdapter->setIdentity($username)
+        ->setCredential($password);
+        $result = $auth->authenticate($authAdapter);
+    
+        if ($result->isValid())
+        {
+            Zwei_Admin_Auth::initUserInfo($authAdapter);
+        } else {
+            echo "Usuario '$username' o Password '$password' incorrectos.";
+        }
+    }
+    
     public function testIndexAction()
     {
+        $this->initUserInfo();
         $params = array('action' => 'index', 'controller' => 'Acl', 'module' => 'default');
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
@@ -20,65 +47,91 @@ class AclControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
-            );
+//         $this->assertQueryContentContains(
+//             'div#view-content p',
+//             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+//             );
     }
 
     public function testIsUserAllowedAction()
     {
-        $params = array('action' => 'isUserAllowed', 'controller' => 'Acl', 'module' => 'default');
-        $urlParams = $this->urlizeOptions($params);
-        $url = $this->url($urlParams);
-        $this->dispatch($url);
+        $this->initUserInfo();
+        $params = array('action' => 'isUserAllowed', 'controller' => 'Acl', 'module' => 'default', 'itemId' => '1');
+
         
         // assertions
-        $this->assertModule($urlParams['module']);
-        $this->assertController($urlParams['controller']);
-        $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
-            );
+        $model = new DbTable_AclModules();
+        $modules = $model->fetchAll("approved='1' AND type='xml' AND module IS NOT NULL");
+        
+        foreach ($modules as $module) {
+            $params['p'] = $module->module;
+            $urlParams = $this->urlizeOptions($params);
+            $url = $this->url($urlParams);
+            $this->dispatch($url);
+            
+            $this->assertModule($urlParams['module']);
+            $this->assertController($urlParams['controller']);
+            $this->assertAction($urlParams['action']);
+        }
+//         $this->assertQueryContentContains(
+//             'div#view-content p',
+//             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+//             );
     }
 
     public function testUserHasRoleAllowedAction()
     {
-        $params = array('action' => 'userHasRoleAllowed', 'controller' => 'Acl', 'module' => 'default');
-        $urlParams = $this->urlizeOptions($params);
-        $url = $this->url($urlParams);
-        $this->dispatch($url);
+        $this->initUserInfo();
+        $params = array('action' => 'userHasRoleAllowed', 'controller' => 'Acl', 'module' => 'default', 'itemId' => '1');
         
         // assertions
-        $this->assertModule($urlParams['module']);
-        $this->assertController($urlParams['controller']);
-        $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
-            );
+        $model = new DbTable_AclModules();
+        $modules = $model->fetchAll("approved='1' AND type='xml' AND module IS NOT NULL");
+        
+        foreach ($modules as $module) {
+            $params['p'] = $module->module;
+            $urlParams = $this->urlizeOptions($params);
+            $url = $this->url($urlParams);
+            $this->dispatch($url);
+            
+            $this->assertModule($urlParams['module']);
+            $this->assertController($urlParams['controller']);
+            $this->assertAction($urlParams['action']);
+        }
+//         $this->assertQueryContentContains(
+//             'div#view-content p',
+//             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+//             );
     }
 
     public function testUserHasGroupsAllowedAction()
     {
+        $this->initUserInfo();
         $params = array('action' => 'userHasGroupsAllowed', 'controller' => 'Acl', 'module' => 'default');
-        $urlParams = $this->urlizeOptions($params);
-        $url = $this->url($urlParams);
-        $this->dispatch($url);
-        
+            
         // assertions
-        $this->assertModule($urlParams['module']);
-        $this->assertController($urlParams['controller']);
-        $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
-            );
+        $model = new DbTable_AclModules();
+        $modules = $model->fetchAll("approved='1' AND type='xml' AND module IS NOT NULL");
+        
+        foreach ($modules as $module) {
+            $params['p'] = $module->module;
+            $urlParams = $this->urlizeOptions($params);
+            $url = $this->url($urlParams);
+            $this->dispatch($url);
+            
+            $this->assertModule($urlParams['module']);
+            $this->assertController($urlParams['controller']);
+            $this->assertAction($urlParams['action']);
+        }
+//         $this->assertQueryContentContains(
+//             'div#view-content p',
+//             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+//             );
     }
 
     public function testGetUserInfoAction()
     {
+        $this->initUserInfo();
         $params = array('action' => 'getUserInfo', 'controller' => 'Acl', 'module' => 'default');
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
@@ -88,22 +141,10 @@ class AclControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
-            );
+//         $this->assertQueryContentContains(
+//             'div#view-content p',
+//             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+//             );
     }
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
