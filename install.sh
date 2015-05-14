1ae617aa73b644f5e56d2ee23e4fe060d8bc0287
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-05-14 21:03:34 +0000

    #817 Upgrade Instalador
    git-svn-id: svn://saruman/admportal/trunk@1049 32ebc6c2-cfed-11df-be44-b108719c1499

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo composer install && sudo composer update && bower install && cp -R dojotoolkit/* bower_components/
+sudo composer install && sudo composer update && bower install && cp -R dojotoolkit/* bower_components/ && cp -n $PWD/vendor/zend/zendframework/bin/zf.sh $PWD/vendor/zend/zendframework/bin/zf
 
 echo "Dependencias instaladas"
 echo " "
@@ -8,16 +8,18 @@ echo "--------------------------------------------------------------------------
 echo "Recuerde agregar estas variable de ambiente a su archivo ~/.bashrc (o equivalente)"
 echo "ZWC_ADMPORTAL=$PWD"
 echo "export PATH=\$ZWC_ADMPORTAL/tools:\$PATH"
-echo "export PATH=/usr/share/php/zend/zendframework/bin:\$PATH"
+echo "export PATH=$PWD/vendor/zend/zendframework/bin:\$PATH"
 echo " "
 echo "----------------------------------------------------------------------------------"
 echo "Apache aliases"
 echo "----------------------------------------------------------------------------------"
 echo "considere los siguientes alias de apache"
-echo "Una buena idea es generar agregar 
-	Include alias/*.conf a su archivo apache.conf o http.conf"
+echo "Una buena idea es generar agregar la línea
+	Include alias/*.conf 
+	al final de su archivo /etc/apache2/apache2.conf o /etc/httpd/httpd.conf"
 echo " "
-echo "dojotoolkit.conf"
+echo "Crear archivo /etc/apache2/alias/dojotoolkit.conf o /etc/httpd/alias/dojotoolkit.conf con el contenido:"
+echo " "
 echo "Alias /dojotoolkit \"$PWD/bower_components\"
 <Directory \"$PWD/bower_components\">
 	Options Indexes FollowSymLinks
@@ -26,7 +28,8 @@ echo "Alias /dojotoolkit \"$PWD/bower_components\"
 </Directory>
 "
 echo " "
-echo "libs.conf"
+echo "Crear archivo /etc/apache2/alias/libs.conf o /etc/httpd/alias/libs.conf con el contenido:"
+echo " "
 echo "Alias /libs \"$PWD/public/js/libs\"
 <Directory \"$PWD/public/js/libs\">
 	Options Indexes FollowSymLinks
@@ -40,7 +43,7 @@ echo "--------------------------------------------------------------------------
 echo "php.ini"
 echo "-----------------------------------------------------------------------------------"
 echo "agregar a la variable \"include_path\" las rutas" 
-echo ":/usr/share/php/zend/zendframework/library:/usr/share/php/phpunit/phpunit/PHPUnit"
+echo ":$PWD/vendor/zend/zendframework/library:$PWD/vendor/phpunit/phpunit"
 echo " "
 
 
