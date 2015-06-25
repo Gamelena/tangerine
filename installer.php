1ac05503ff57b574ff26b4452a8fb1dc851f9c31
carlos <carlos@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-06-25 16:21:17 +0000

    #817 Via composer hago los agjustes de dojo que originalmente se hacian en install.sh
    git-svn-id: svn://saruman/admportal/trunk@1083 32ebc6c2-cfed-11df-be44-b108719c1499

--- installer.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-passthru( "/opt/composer/bower/bin/bower install --allow-root");
-passthru( "cp -R dojotoolkit/* bower_components/");
-passthru( "cp -R bower_components/dojo-calendar/* bower_components/dojox/calendar/");
-
