2d9380a85165195dbf1f1c057171f84fc9736657
rodrigo <rodrigo.riquelme@zweicom.com>
2015-08-27 18:46:14 -0300

    modulo extradata, upgrade json select

--- application/modules/components/controllers/ExtraDataController.php
+++ application/modules/components/controllers/ExtraDataController.php
@@ -3,14 +3,91 @@
 class Components_ExtraDataController extends Zend_Controller_Action
 {
 
-    public function init()
+    /**
+     *
+     * @var Zwei_Db_Table
+     *
+     *
+     */
+    protected $_model = null;
+    
+    /**
+     *
+     * @var Zwei_Admin_Acl
+     *
+     *
+     */
+    protected $_acl = null;
+    
+    /**
+     *
+     * @var Zwei_Admin_Xml
+     *
+     *
+     */
+    protected $_xml = null;
+    
+    /**
+     * Post Constructor
+     *
+     * @see Zend_Controller_Action::init()
+     */
+    public function init ()
     {
-        /* Initialize action controller here */
+        $r = $this->getRequest();
+        $component = $r->getParam('p');
+        $file = Zwei_Admin_Xml::getFullPath($component);
+        $this->_xml = new Zwei_Admin_Xml($file, 0, 1);
+        
+        if ($this->_xml->getAttribute('target')) {
+            $this->view->model = $this->view->editableModel = $this->_xml->getAttribute('target');
+            $this->view->component = $component;
+            $this->view->domPrefix = Zwei_Utils_String::toVarWord($this->view->component);
+            $this->view->extraDataParams = array();
+            $this->view->idParentEditorName = null;
+    
+            $search = $r->getParam('search', array());
+    
+            //yes, se espera solo un elemento
+            foreach ($search as $id => $s) {
+                $this->view->idExtraDataName = $id;
+                $this->view->idExtraDataValue = $s['value'] ? $s['value'] : '0';
+            }
+    
+            $this->_model = new $this->view->model();
+    
+            $this->_acl = new Zwei_Admin_Acl();
+        } else {
+            throw new Zwei_Exception("$component no tiene atributo target válido");
+        }
     }
 
-    public function indexAction()
+    /**
+     * Acción Listar Valores de Variables Extra Data
+     *
+     * @return void
+     */
+    public function indexAction ()
     {
-        // action body
+        $r = $this->getRequest();
+        $this->initPermissions();
+    
+        $this->view->editableModel = $this->_xml->getAttribute('target');
+        $this->view->idParentEditorName = $r->getParam('idParentEditorName');
+    }
+
+    /**
+     * Inicializar flags de acceso para las vistas
+     * 
+     * @return void
+     */
+    private function initPermissions ()
+    {
+        $component = $this->_xml->getAttribute('aclComponent');
+        $aclComponent = $component ? $component : $this->getRequest()->getParam('p');
+        
+        $this->view->isAllowedAdd = $this->_acl->isUserAllowed($aclComponent, 'ADD') && $this->_xml->getAttribute('add') === 'true';
+        $this->view->isAllowedDelete = $this->_acl->isUserAllowed($aclComponent, 'DELETE') && $this->_xml->getAttribute('add') === 'true';
     }
 
 
