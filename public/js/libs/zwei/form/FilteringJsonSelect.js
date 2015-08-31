c9e0ebcbf3308ed2815a9d7ba121f982a0076b86
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-08-31 20:25:38 +0000

    #817 unbounded arrays in zwei.form.FilteringJsonSelect
    git-svn-id: svn://saruman/admportal/trunk@1152 32ebc6c2-cfed-11df-be44-b108719c1499

--- public/js/libs/zwei/form/FilteringJsonSelect.js
+++ public/js/libs/zwei/form/FilteringJsonSelect.js
@@ -14,7 +14,7 @@ define(["dojo/_base/declare", "dijit/form/FilteringSelect"], function(declare, F
 				handleAs : 'json',
 				load : function(items) {
 					self.items = items;
-					self.refreshValues(items)
+					self.refreshValues(items);
 				},
 				error : function(e) {
 					utils.showMessage(e.message, 'error');
@@ -27,21 +27,33 @@ define(["dojo/_base/declare", "dijit/form/FilteringSelect"], function(declare, F
 			this.refreshValues();
 			dojo.connect(this.domNode, 'onkeyup', this, 'refreshValues');
 		},
-		refreshValues : function() {
+		refreshValues : function(evt) {
 			var url = this.url;
 			var text = this.textbox.value;
-			this.iterateLevels(text, this.items);
+			this.iterateLevels(text, this.items, evt);
 			//self.set('store', self.store);
 		},
-		iterateLevels : function(text, items) {
+		iterateLevels : function(text, items, evt) {
 			var self = this;
 			var levels = text.split(".");
 			var isDot = text[text.length - 1] === ".";
 			var isLeftSquareBracket = text[text.length - 1] === "[";
 			var data = [];
+			var isIndex = false;
 			var basePath = '';
-			require(["dojo/store/Memory", "dojox/json/query"], function(Memory, query) {
-				if (isDot || isLeftSquareBracket) {
+			
+			require(["dojo/store/Memory", "dojox/json/query", "dojo/keys"], function(Memory, query, keys) {
+				if (self.unbounded) {
+					isIndex = text.match("\\[([0-9]+)$");
+					if (isIndex) {
+						data.push({
+							id : text + "]",
+							name : text + "]"
+						});
+					}
+				}
+
+				if (isDot || isLeftSquareBracket || (typeof evt !== 'undefined' && evt.keyCode == dojo.keys.BACKSPACE)) {
 					var basePath = '';
 					var value = '';
 					dojo.forEach(levels, function(piece) {
@@ -52,19 +64,32 @@ define(["dojo/_base/declare", "dijit/form/FilteringSelect"], function(declare, F
 						}
 					});
 					var basePath = levels.join(".");
-					var results = query("$." + basePath.slice(0, -1), items);
+
+					if (self.unbounded && isDot && text.match("^(.*)\\[.*")) {
+						var value = text.match("^(.*)\\[.*")[1];
+						
+						if (evt.keyCode == dojo.keys.BACKSPACE) {
+							console.log('que no haga boing');
+						}
+
+
+						var results = query("$." + value + '[0]', items);
+					} else {
+						var results = query("$." + basePath.slice(0, -1), items);
+					}
+
 					var first = true;
 					var firstIndex = null;
 					for (var index in results) {
-						if ( typeof results === 'object') {
-							if ( results instanceof Array) {
+						if (typeof results === 'object') {
+							if (results instanceof Array) {
 								value = basePath.substring(0, basePath.length - 1) + "[" + index + "]";
 								data.push({
 									id : value,
 									name : value
 								});
 
-								if (!first) {
+								if (first) {
 									firstIndex = index;
 									first = false;
 								}
