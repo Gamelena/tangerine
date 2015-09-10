62280276c775df9962e8c749d4716d8527c22a22
rodrigo <rodrigo@32ebc6c2-cfed-11df-be44-b108719c1499>
2015-09-10 18:21:52 +0000

    #817 Dummy Adapter para usar en modelos no DB
    git-svn-id: svn://saruman/admportal/trunk@1163 32ebc6c2-cfed-11df-be44-b108719c1499

---
+++ library/Zwei/Db/Dummy/Adapter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * Adaptador dummy, útil para implementar en Modelos No DB que usen la api rest CrudRequestController.
+ * @example
+ * <code>
+ * class MyCrazyModel implements Zwei_Admin_ModelInterface
+ * {
+ *     public function getAdapter()
+ *     {
+ *         return new Zwei_Db_Dummy_Adapter(); 
+ *     }
+ * }
+ * </code>
+ */
+class Zwei_Db_Dummy_Adapter
+{
+    public function quoteInto($text, $value, $type = null, $count = null)
+    {
+        if ($count === null) {
+            return str_replace('?', $this->quote($value, $type), $text);
+        } else {
+            while ($count > 0) {
+                if (strpos($text, '?') !== false) {
+                    $text = substr_replace($text, $this->quote($value, $type), strpos($text, '?'), 1);
+                }
+                --$count;
+            }
+            return $text;
+        }
+    }
+    
+    public function quote($value, $type = null)
+    {
+    
+        return "'" . str_replace("'", "\'", $value) . "'";
+    }
+    
+    public function quoteIdentifier($value)
+    {
+    
+        return $this->quote($value);
+    }
+}
