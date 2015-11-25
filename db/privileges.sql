491166a1f813835579e48ed94606f8050bfb02b6
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2015-11-25 14:53:57 -0300

    Delete privileges.sql

--- db/privileges.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `admportal`;
-
-USE `admportal`;
-
-GRANT ALL ON admportal.* to admportal_user identified by 'admportal_pass';
-GRANT ALL ON admportal.* to admportal_user@localhost identified by 'admportal_pass';
