5132d11e2ba637ef13a5d92857cb2f39a1248511
rriquelme <rodrigo.riquelme.e@gmail.com>
2016-04-11 14:28:17 -0300

    Install phploc from dev-master

--- tasks/user-testing-deps.sh
+++ tasks/user-testing-deps.sh
@@ -1,5 +1,5 @@
 composer global require 'squizlabs/php_codesniffer=*'
-composer global require 'phploc/phploc=*'
+composer global require 'phploc/phploc=dev-master'
 composer global require 'pdepend/pdepend=*'
 composer global require 'phpmd/phpmd=*'
 composer global require 'phpdocumentor/phpdocumentor:2.*'
