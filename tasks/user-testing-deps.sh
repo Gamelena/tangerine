f7ec658e320ffc44d12cffab113e4b6f5f913d2f
rriquelme <rodrigo.riquelme.e@gmail.com>
2016-04-11 17:08:36 -0300

    looking 4 phploc

--- tasks/user-testing-deps.sh
+++ tasks/user-testing-deps.sh
@@ -4,5 +4,7 @@ composer global require 'pdepend/pdepend=*'
 composer global require 'phpmd/phpmd=*'
 composer global require 'phpdocumentor/phpdocumentor:2.*'
 composer global require 'sebastian/phpcpd=*'
-
+cd ..
+export PATH="./vendor/bin:.:$PATH"
+cd -
 
