2a2f86858b436f7233dd3b73458ab1a7d9882874
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2016-04-11 10:01:04 -0300

    user testing deps shell

---
+++ tasks/user-testing-deps.sh
@@ -0,0 +1,7 @@
+composer global require 'squizlabs/php_codesniffer=*'
+composer global require 'phploc/phploc=*'
+composer global require 'pdepend/pdepend=*'
+composer global require 'phpmd/phpmd=*'
+composer global require 'phpdocumentor/phpdocumentor:2.*'
+composer global require 'sebastian/phpcpd=*'
+
