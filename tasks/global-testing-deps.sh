a9d79cbad776960d2723b2cf4f7d18cce06eef4d
Rodrigo Riquelme <rodrigo.riquelme.e@gmail.com>
2016-04-18 14:44:45 -0300

    global testing deps

--- tasks/global-testing-deps.sh
+++ tasks/global-testing-deps.sh
@@ -1,33 +1,28 @@
-if [ "$(id -u)" != "0" ]; then
-	echo "This script must be run as root or sudo" 1>&2
-	exit 1
-else 
-	wget -c https://www.phpunit.de/phpunit-4.8.18.phar
-	chmod +x phpunit-4.8.18.phar
-	mv phpunit-4.8.18.phar /usr/bin/phpunit
+wget -c https://www.phpunit.de/phpunit-4.8.18.phar
+chmod +x phpunit-4.8.18.phar
+mv phpunit-4.8.18.phar /usr/bin/phpunit
 
-	wget -c https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-	chmod +x phpcs.phar
-	mv phpcs.phar /usr/bin/phpcs
+wget -c https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+chmod +x phpcs.phar
+mv phpcs.phar /usr/bin/phpcs
 
-	wget -c https://phar.phpunit.de/phploc.phar
-	chmod +x phploc.phar
-	mv phploc.phar /usr/bin/phploc
+wget -c https://phar.phpunit.de/phploc.phar
+chmod +x phploc.phar
+mv phploc.phar /usr/bin/phploc
 
-	wget -c http://static.pdepend.org/php/latest/pdepend.phar
-	chmod +x pdepend.phar
-	mv pdepend.phar /usr/bin/pdepend
+wget -c http://static.pdepend.org/php/latest/pdepend.phar
+chmod +x pdepend.phar
+mv pdepend.phar /usr/bin/pdepend
 
-	wget -c http://static.phpmd.org/php/latest/phpmd.phar
-	chmod +x phpmd.phar
-	mv phpmd.phar /usr/bin/phpmd
+wget -c http://static.phpmd.org/php/latest/phpmd.phar
+chmod +x phpmd.phar
+mv phpmd.phar /usr/bin/phpmd
 
-	wget https://github.com/theseer/phpdox/releases/download/0.8.0/phpdox-0.8.0.phar
-	chmod +x phpdox-0.8.0.phar
-	mv phpdox-0.8.0.phar /usr/bin/phpdox
+wget https://github.com/theseer/phpdox/releases/download/0.8.0/phpdox-0.8.0.phar
+chmod +x phpdox-0.8.0.phar
+mv phpdox-0.8.0.phar /usr/bin/phpdox
 
-	wget https://phar.phpunit.de/phpcpd.phar
-	chmod +x phpcpd.phar
-	mv phpcpd.phar /usr/bin/phpcpd
-fi
+wget https://phar.phpunit.de/phpcpd.phar
+chmod +x phpcpd.phar
+mv phpcpd.phar /usr/bin/phpcpd
 
