7490e772369ae9f06a281ae5fc947adfadd9cc27
donmostro <rodrigo.riquelme.e@gmail.com>
2016-04-17 23:41:56 -0300

    travis test

--- tasks/user-testing-deps.sh
+++ tasks/user-testing-deps.sh
@@ -1,11 +1,4 @@
-composer global require 'squizlabs/php_codesniffer=*'
-composer global require 'phploc/phploc=dev-master'
-composer global require 'pdepend/pdepend=*'
-composer global require 'phpmd/phpmd=*'
-composer global require 'phpdocumentor/phpdocumentor:2.*'
-composer global require 'sebastian/phpcpd=*'
 cd ..
-export PATH="$HOME/vendor/bin:$PATH"
+export PATH="./vendor/bin:.:$PATH"
 cd -
-echo $PATH
 
