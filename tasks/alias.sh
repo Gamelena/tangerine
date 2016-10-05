d46ba8c8b74e45afdca5a7c0e6798a93232ecd3b
Rodrigo Riquelme <rodrigo.riquelme@2brains.cl>
2016-10-05 16:34:54 -0300

    alias osx

--- tasks/alias.sh
+++ tasks/alias.sh
@@ -3,8 +3,17 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 else 
 	ROOT_DIR="../"
-	TANGERINEPATH=`echo $(dirname $(readlink -e $PWD))`
+	unamestr=`uname`
+
+	if [[ "$unamestr" == 'Darwin' ]]; then
+		echo "Se instentara usar greadlink, de no estar disponible debe instalar el paquete coreutils"
+    	TANGERINEPATH=`echo $(dirname $(greadlink -f $PWD))`
+	else
+		TANGERINEPATH=`echo $(dirname $(readlink -e $PWD))`
+	fi
+
 	APACHE_EXEC=$(which apache2)
+
 	if [ ! -x "$APACHE_EXEC" ]; then
 		APACHE_EXEC=$(which httpd)
 		if [ ! -x "$APACHE_EXEC" ]; then
@@ -12,13 +21,22 @@ else
 			exit 1;
 		else
 			APACHE_SERVICE="httpd"
-			APACHE_PATH="/etc/httpd"
-			APACHE_CONF="/etc/httpd/conf/httpd.conf"
+			if [[ "$unamestr" == 'Darwin' ]]; then 
+				echo "Detectado OS X"
+				APACHE_PATH="/etc/apache2"
+				APACHE_CONF="/etc/apache2/httpd.conf"
+				APACHE_USER="_www"
+			else
+				APACHE_PATH="/etc/httpd"
+				APACHE_CONF="/etc/httpd/conf/httpd.conf"
+				APACHE_USER="apache"
+			fi
 		fi
 	else
 		APACHE_SERVICE="apache2"
 		APACHE_PATH="/etc/apache2"
 		APACHE_CONF="/etc/apache2/apache2.conf"
+		APACHE_USER="www-data"
 	fi
 	
 	mkdir -p $APACHE_PATH/alias
@@ -32,7 +50,7 @@ else
 
     		echo "Se intentará agregar la directiva Include alias/*.conf en $APACHE_CONF"
 		echo "# Generado por AdmPortal" >> $APACHE_CONF
-		echo "Include alias/*.conf\n" >> $APACHE_CONF
+		echo "Include $APACHE_PATH/alias/*.conf\n" >> $APACHE_CONF
 	fi
 
 	APACHE_VERSION=`$APACHE_SERVICE -v | grep -o '2\.[0-9]*'` 
